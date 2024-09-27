eaa1754b14d258fcba91ec853fec4c49a82be8bf
hapdev05 <demoover81@gmail.com>
2024-09-28 01:42:50 +0700

    HandleLogin

--- ConnectDB.js
+++ ConnectDB.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 7749;
 const cors = require("cors");
 const mysql = require("mysql");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 
 // Middleware
-app.use(cors()); // Chấp nhận kết nối từ React
+app.use(
+  cors({
+    origin: "*", // Cho phép tất cả các nguồn
+    methods: ["GET", "POST", "PUT", "DELETE"], // Các phương thức HTTP được cho phép
+    allowedHeaders: ["Content-Type", "Authorization"], // Các headers được cho phép
+  })
+);
 app.use(express.json()); // Parse JSON từ React
+app.use(bodyParser.json()); // Parse JSON
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Kết nối MySQL
 var connectDB = mysql.createConnection({
   host: "localhost",
   user: "root",
+
   password: "", // Nếu có mật khẩu, thay thế chuỗi rỗng bằng mật khẩu của bạn
   database: "dacs2",
 });
+
+const secrecKey =
+  "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.VFb0qJ1LRg_4ujbZoRMXnVkUgiuKq5KxWqNdbKq_G9Vvz-S1zZa9LPxtHWKa64zDl2ofkT8F6jBt_K4riU-fPg"; // Thay thế với secret key của bạn
 
 connectDB.connect(function (err) {
   if (err) {
@@ -27,6 +42,58 @@ connectDB.connect(function (err) {
 // Route mẫu để kiểm tra kết nối
 app.get("/", (req, res) => {
   res.send("Server is running and connected to MySQL!");
+});
+
+// API đăng nhập
+app.post("/login", (req, res) => {
+  // Lấy email và password từ request body
+  const { email, password } = req.body;
+  console.log("Email:", email);
+  console.log("Password:", password);
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Vui lòng nhập email và mật khẩu" });
+  }
+
+  const query = "SELECT * FROM account WHERE email = ?";
+  connectDB.query(query, [email], (err, result) => {
+    if (err) throw err;
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Email không hợp lệ" });
+    }
+
+    const user = result[0];
+
+    // So sánh mật khẩu
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (!isMatch) {
+        return res.status(401).json({ message: "Mật khẩu không chính xác" });
+      }
+
+      // Tạo JWT
+      const token = jwt.sign({ id: user.id }, secrecKey, { expiresIn: "1h" });
+
+      // Lấy địa chỉ IP và chuyển đổi nếu cần
+      let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+      if (ip === "::1") {
+        ip = "127.0.0.1"; // Chuyển đổi IPv6 sang IPv4
+      }
+      console.log("Địa chỉ IP:", ip);
+
+      // Trả về thông tin và token
+      res.json({
+        message: "Đăng nhập thành công!",
+        token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        ip: ip,
+      });
+    });
+  });
 });
 
 // Khởi động server
