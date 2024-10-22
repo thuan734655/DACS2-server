6e5cf61e12cf81e7a65aeafb1fcf8a36462328d0
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-10-22 12:42:42 +0700

    #3

--- Router/loginRoute.js
+++ Router/loginRoute.js
@@ -16,7 +16,6 @@ routerLogin.use(bodyParser.json());
 const secrecKey =
   "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.VFb0qJ1LRg_4ujbZoRMXnVkUgiuKq5KxWqNdbKq_G9Vvz-S1zZa9LPxtHWKa64zDl2ofkT8F6jBt_K4riU-fPg"; // Replace with your secret key
 
-// Login API
 routerLogin.post("/login", async (req, res) => {
   const { email, password, ip } = req.body;
 
