a971171d738e9b5b066b8c2406c50e0e6e922fba
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-12-09 13:15:18 +0700

    setup db

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -1,13 +1,16 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dacs2",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "dacs2",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 export default connectDB;
