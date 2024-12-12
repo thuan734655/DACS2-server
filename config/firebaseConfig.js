ee38ba822726c998f20a249965c41383d1c8b710
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-12-13 03:14:27 +0700

    fix deploy #6

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,12 +3,32 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Check for required environment variables
+const requiredEnvVars = [
+  'FIREBASE_ADMIN_TYPE',
+  'FIREBASE_ADMIN_PROJECT_ID',
+  'FIREBASE_ADMIN_PRIVATE_KEY_ID',
+  'FIREBASE_ADMIN_PRIVATE_KEY',
+  'FIREBASE_ADMIN_CLIENT_EMAIL',
+  'FIREBASE_ADMIN_CLIENT_ID',
+  'FIREBASE_ADMIN_AUTH_URI',
+  'FIREBASE_ADMIN_TOKEN_URI',
+  'FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL',
+  'FIREBASE_ADMIN_CLIENT_X509_CERT_URL'
+];
+
+const missingEnvVars = requiredEnvVars.filter(varName => !process.env[varName]);
+
+if (missingEnvVars.length > 0) {
+  throw new Error(`Missing required environment variables: ${missingEnvVars.join(', ')}`);
+}
+
 // Create service account configuration from environment variables
 const serviceAccount = {
   type: process.env.FIREBASE_ADMIN_TYPE,
   project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
   private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY ? process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
   client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
   auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
