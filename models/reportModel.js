51e2b03ccecb48ecebaf7d1a8b0935f1ea207633
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-12-23 02:24:26 +0700

    ok get and display report

--- models/reportModel.js
+++ models/reportModel.js
@@ -5,7 +5,6 @@ class ReportModel {
     try {
       const reportRef = db.ref("reports");
       const reportKey = reportRef.push().key;
-
       const newReport = {
         idReport: reportKey,
         postId: content.postId || "",
@@ -13,6 +12,7 @@ class ReportModel {
         replyId: content.replyId || "",
         reason: content.reason || "",
         type: content.type || "",
+        status: "PENDING",
         createdAt: new Date().toISOString(),
       };
 
@@ -29,11 +29,42 @@ class ReportModel {
     try {
       const snapshot = await db.ref("reports").once("value");
       const reports = snapshot.val() || {};
-      console.log("All reports:", reports);
-      return Object.values(reports);
+      const result = await Promise.all(
+        Object.entries(reports).map(async ([key, value]) => {
+          console.log("value", value);
+          let relatedData = null;
+
+          if (value.type === "POST" && value.postId) {
+            // Lấy thông tin chi tiết bài viết
+            const postSnapshot = await db
+              .ref(`posts/${value.postId}`)
+              .once("value");
+            relatedData = postSnapshot.val();
+          } else if (value.type === "COMMENT" && value.commentId) {
+            // Lấy thông tin chi tiết bình luận
+            const commentSnapshot = await db
+              .ref(`commentsList/${value.commentId}`)
+              .once("value");
+            relatedData = commentSnapshot.val();
+          }
+
+          // Trả về báo cáo kèm thông tin liên quan
+          return {
+            idReport: key,
+            ...value,
+            relatedData, // Thông tin chi tiết của post/comment
+          };
+        })
+      );
+
+      console.log(
+        "Fetched all reports successfully with related data:",
+        result
+      );
+      return { success: true, reports: result };
     } catch (error) {
       console.error("Error getting reports:", error);
-      throw error;
+      return { success: false, error: error.message };
     }
   }
 }
